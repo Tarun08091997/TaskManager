d35d1c3523ba2f16fa72acc37bfafeb1880501ff
Tarun Kumar <tarunsharma080997@gmail.com>
2024-06-05 17:30:08 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import AddTask from './components/AddTask'
+
+function App() {
+
+  return (
+    <div className=' bg-white w-full h-[100vh]'>
+        <AddTask />       
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/AddTask.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function AddTask() {
+  return (
+    <div className='w-[80%] md:w-[500px] lg:w-[650px] h-[44px] absolute top-[20%] left-1/2 -translate-x-1/2'>
+        <input type='text' 
+        className=' border-[2px] border-black w-full h-full'/>
+    </div>
+  )
+}
