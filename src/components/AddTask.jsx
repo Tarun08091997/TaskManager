cf529c7dab451c0c49df63f34ffed1aa9d39f451
Tarun Kumar <tarunsharma080997@gmail.com>
2024-06-06 17:20:50 +0530

    Crate Task Card

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -2,9 +2,15 @@ import React from 'react'
 
 export default function AddTask() {
   return (
-    <div className='w-[80%] md:w-[500px] lg:w-[650px] h-[44px] absolute top-[20%] left-1/2 -translate-x-1/2'>
+    <div className='w-[80%] md:w-[500px] lg:w-[650px] h-[44px] 
+    absolute top-[20%] left-1/2 -translate-x-1/2 self-center'>
         <input type='text' 
-        className=' border-[2px] border-black w-full h-full'/>
+        className='w-[70%] rounded-l-xl h-full outline-none bg-gray-300 text-[15px] text-center'/>
+        <input type='number' className='w-[15%] h-full 
+        hover:cursor-pointer text-center font-bold text-[11px] md:text-[15px] outline-none' placeholder='Task Order'/>
+        <button className={`h-full w-[15%] rounded-r-xl font-bold text-[15px]
+         bg-blue-500 text-white 
+         box-border hover:shadow-lg hover:shadow-black`}>Create</button>
     </div>
   )
 }
