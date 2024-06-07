a54483b631c1296a99f8cd58c69db3ce85475fac
Tarun Kumar <tarunsharma080997@gmail.com>
2024-06-07 17:24:18 +0530

    Task Card in working

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
+import { useState } from 'react';
 import './App.css'
 import AddTask from './components/AddTask'
+import TaskCard from './components/TaskCard';
 
 function App() {
-
+  const [task , setTask] = useState([]);
+  const createTask = (newTask) =>{
+    console.log(newTask);
+  }
   return (
     <div className=' bg-white w-full h-[100vh]'>
-        <AddTask />       
+        <AddTask createTask = {createTask}/>
+        {task.map((item,index) => {
+          <TaskCard key = {index}/>
+        })}
+
     </div>
   )
 }
